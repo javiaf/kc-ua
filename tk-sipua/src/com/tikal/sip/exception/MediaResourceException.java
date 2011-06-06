ea49296bd87742bad83289f526cbac219603148d
Qiang Zhow <qiang.qt@gmail.com>
2011-06-06 14:29:35 +0000

    Resource exception

--- tk-sipua/src/com/tikal/sip/exception/MediaResourceException.java
+++ tk-sipua/src/com/tikal/sip/exception/MediaResourceException.java
@@ -1,5 +1,33 @@
 package com.tikal.sip.exception;
 
-public class MediaResourceException extends Exception {
 
+/**
+ * Global exception for resources errors.
+ * @author qiang
+ *
+ */
+public class MediaResourceException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructs a MediaResourceException
+	 * 
+	 * @param message
+	 *            The message with the Exception
+	 */
+	public MediaResourceException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a MediaResourceException
+	 * 
+	 * @param message
+	 *            The message with the Exception
+	 * @param cause
+	 *            The cause of the Exception
+	 */
+	public MediaResourceException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
